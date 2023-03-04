cd5f30264caad5da82b73b3bc77fc83b78bb93fa
Zyf <2069708864@qq.com>
2023-03-04 10:49:57 +0800

    second commit

---
+++ supermarket_back/src/main/java/com/example/supermarket_back/controller/HelloWord.java
@@ -0,0 +1,9 @@
+package com.example.supermarket_back.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloWord {
+}
